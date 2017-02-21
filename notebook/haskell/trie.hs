992688830d17a7c72ddac32ab9d815af20d5acf6
Yicheng Wang <alex.wyc2098@protonmail.ch>
2017-02-20 21:46:16 -0500

    add remove

--- notebook/haskell/trie.hs
+++ notebook/haskell/trie.hs
@@ -3,7 +3,7 @@ import Data.Map (Map)
 import qualified Data.Map as Map
 import Control.Monad
 
-data Trie a = Trie Int (Map a (Trie a))
+data Trie a = Null | Trie Int (Map a (Trie a))
 
 size :: Trie a -> Int -- number of words / arrays stores in the trie
 size (Trie i m)
@@ -18,12 +18,25 @@ insert (x:xs) (Trie i m) = Trie i $ Map.insert x updatedChild m
           child = Map.findWithDefault def x m
           def = Trie 0 Map.empty
 
+remove :: (Ord a) => [a] -> Trie a -> Maybe (Trie a)
+remove [] (Trie 1 m) = if Map.size m == 0
+    then Nothing
+    else Just (Trie 0 m)
+remove (x:xs) (Trie i m) = Just (Trie i updatedM)
+    where updatedM = Map.update (remove xs) x m
+
 countPrefix :: (Ord a) => [a] -> Trie a -> Int
 countPrefix [] (Trie i m)
     | Map.size m == 0   = i
     | otherwise         = i + Map.fold scf 0 m
         where scf a b = b + (countPrefix [] a)
 countPrefix (x:xs) (Trie i m) = maybe 0 (countPrefix xs) (Map.lookup x m)
+
+member :: (Ord a) => [a] -> Trie a -> Bool
+member [] (Trie i m) = (i > 0)
+member (x:xs) (Trie i m) = maybe False (member xs) (Map.lookup x m)
+
+-- I/O and main thread stuff
 
 buildTrieFromInput :: IO (Trie Char)
 buildTrieFromInput = go (Trie 0 Map.empty)
@@ -38,6 +51,16 @@ queryResults t = do query <- getLine
                         putStrLn $ show $ countPrefix query t
                         queryResults t
 
+
+deleteFromTrie :: (Trie Char) -> IO (Trie Char)
+deleteFromTrie t = do
+    word <- getLine
+    if word == ""
+       then return t
+       else maybe (return $ Trie 0 Map.empty) deleteFromTrie (remove word t)
+
 main :: IO()
 main = do builtTrie <- buildTrieFromInput
           queryResults builtTrie
+          updatedTrie <- deleteFromTrie builtTrie
+          queryResults updatedTrie
